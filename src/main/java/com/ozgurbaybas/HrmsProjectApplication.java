6798e28b041c916c287d3f5427ff3933a68a1bf1
codeprex <ozgurbaybas@gmail.com>
2022-12-31 00:10:20 +0300

    SpringDoc OpenAPI UI implementation completed

--- src/main/java/com/ozgurbaybas/HrmsProjectApplication.java
+++ src/main/java/com/ozgurbaybas/HrmsProjectApplication.java
@@ -1,7 +1,13 @@
 package com.ozgurbaybas;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class HrmsProjectApplication {
@@ -10,4 +16,13 @@ public class HrmsProjectApplication {
 		SpringApplication.run(HrmsProjectApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+		return new OpenAPI()
+				.info(new Info()
+				.title("Hrms Project API")
+				.version(appVersion)
+				.description(appDesciption)
+				.license(new License().name("Hrms Project Licence")));
+	}
 }
